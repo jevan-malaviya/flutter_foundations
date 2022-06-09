aae81f597127384964d1c449af2fba2013f513d0
Jevan Malaviya <jevan.malaviya@gmail.com>
2022-06-09 13:00:51 -0400

    changeNotifier with riverpod

--- lib/randomzier_page.dart
+++ lib/randomzier_page.dart
@@ -3,32 +3,32 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_foundations/randomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_foundations/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RandomizerPage extends StatelessWidget {
+class RandomizerPage extends ConsumerWidget {
   RandomizerPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomizer = ref.watch(randomizerProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Randomizer'),
       ),
-      body: Center(child: Consumer<RandomizerChangeNotifier>(
-        builder: (context, notifier, child) {
-          return Text(
-            notifier.generatedNumber?.toString() ?? 'Generate a Number',
-            style: TextStyle(fontSize: 42),
-          );
-        },
-      )),
+      body: Center(
+        child: Text(
+          randomizer.generatedNumber?.toString() ?? 'Generate a Number',
+          style: TextStyle(fontSize: 42),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
           label: Text('Generate'),
           onPressed: () {
-            context.read<RandomizerChangeNotifier>().generateNumber();
+            ref.read(randomizerProvider).generateNumber();
           }),
     );
   }
